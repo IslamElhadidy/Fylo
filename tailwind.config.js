69c61e295e710bc8ddcb5b0b1b3dd3fd427d166f
Eslam Elhadidi <you@example.com>
2023-02-19 08:46:37 +0200

    upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,10 @@ module.exports = {
         opensans: ['Open Sans','san-serif'],
       },
       backgroundImage: (theme) =>  ({
-        'logo-dark-mode' : "url('/images/logo-dark-mode.svg')",
-        'logo-light-mode' : "url('/images/logo-light-mode.svg')",
-        'curvy-dark-mode' : "url('/images/bg-curvy-dark-mode.svg')",
-        'curvy-light-mode' : "url('/images/bg-curvy-light-mode.svg')"
+        'logo-dark-mode' : "url('images/logo-dark-mode.svg')",
+        'logo-light-mode' : "url('images/logo-light-mode.svg')",
+        'curvy-dark-mode' : "url('images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode' : "url('images/bg-curvy-light-mode.svg')"
       })
     },
   },
